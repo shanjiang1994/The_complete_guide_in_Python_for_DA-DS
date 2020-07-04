4ea16ae4a66589de6cf6887461c4cd6b06727be0
Shan_Jiang <32196951+devin19940107@users.noreply.github.com>
2020-07-04 03:35:17 -0400

    update

--- py_files/daily practice.py
+++ py_files/daily practice.py
@@ -1,6 +1,10 @@
-nums1  = [1,2,3,4]
-nums2 = [3,5,7,9]
-for i in nums2:
-    nums1.append(i)
-nums1.sort
-nums1
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+golbal_max, local_max = 0,0
+
+if max(nums) < 0:
+    golbal_max = max(nums)
+else:
+    for num in nums:
+        local_max = max(0, local_max + num)
+        golbal_max = max (golbal_max, local_max)
