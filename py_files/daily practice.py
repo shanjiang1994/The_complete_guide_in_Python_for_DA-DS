9355f3edd7351a772e91db19951e22020f048a0a
Shan_Jiang <32196951+devin19940107@users.noreply.github.com>
2020-07-17 12:24:21 -0400

    UPDATE

--- py_files/daily practice.py
+++ py_files/daily practice.py
@@ -1,25 +1,17 @@
-def quick_sort(nums):
-    n = len(nums)
 
-    def quick(left, right):
-        if left >= right:
-            return nums
-        pivot = right
-        i = left
-        j = right
-        while i < j:
-            while i < j and nums[j] > nums[pivot]:
-                j -= 1
-            while i < j and nums[i] <= nums[pivot]:
-                i += 1
-            nums[i], nums[j] = nums[j], nums[i]
-        nums[pivot], nums[j] = nums[j], nums[pivot]
-        quick(left, j - 1)
-        quick(j + 1, right)
-        return nums
 
-    return quick(0, n - 1)
 
-a = [10,23,51,18,4,31,5,13]
 
-quick_sort(a)
+citations = [9,4,9,8,4]
+def hIndex(citations):
+    n = len(citations)
+    c = sorted(citations)
+
+
+    for i in range(n):
+        if c[i] >= n-i:
+            print(n-i) 
+    print(0)
+
+
+hIndex(citations)
